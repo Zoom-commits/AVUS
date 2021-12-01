4026c441b44100571921b5f58b4f8392d9345f3f
Zoom-commits <61853815+Zoom-commits@users.noreply.github.com>
2021-12-01 13:48:46 -0500

    codigo

---
+++ Codigos/TransmisorAutowifi/Botones.ino
@@ -0,0 +1,18 @@
+const int b2=22, b1=21, b3=2, b4=33, b5=34, b6=35;
+
+
+void setBotones(){
+pinMode(b1, INPUT); 
+pinMode(b2, INPUT); 
+pinMode(b3, INPUT); 
+pinMode(b4, INPUT); 
+pinMode(b5, INPUT);
+pinMode(b6, INPUT);   
+}
+
+void activarBotones(){
+// if(digitalRead(b1)){
+//  llavesitas(94);
+// }
+
+}
